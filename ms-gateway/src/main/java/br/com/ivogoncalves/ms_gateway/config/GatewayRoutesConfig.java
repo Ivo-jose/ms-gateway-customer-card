657a805cc806cc7db2958f1de738143b2b6a9218
Ivo Goncalves <ivojmgs19@gmail.com>
2025-04-19 17:01:01 -0300

    API Gateway created

---
+++ ms-gateway/src/main/java/br/com/ivogoncalves/ms_gateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,20 @@
+package br.com.ivogoncalves.ms_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+	
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("ms-customer", r -> r.path("/api/customers/**")
+						.uri("lb://ms-customer"))
+				.build();
+		
+	}
+}

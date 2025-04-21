2172cffd7f5a72ca79ff04c6d799f51cf502f169
Ivo Goncalves <ivojmgs19@gmail.com>
2025-04-21 16:08:33 -0300

    Removing white space

--- ms-gateway/src/main/java/br/com/ivogoncalves/ms_gateway/config/GatewayRoutesConfig.java
+++ ms-gateway/src/main/java/br/com/ivogoncalves/ms_gateway/config/GatewayRoutesConfig.java
@@ -8,13 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayRoutesConfig {
 
-	
 	@Bean
 	RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route("ms-customer", r -> r.path("/api/customers/**").uri("lb://ms-customer"))
 				.route("ms-cards", r -> r.path("/api/cards/**").uri("lb://ms-cards"))
-				.build();
-		
+				.build();		
 	}
 }
